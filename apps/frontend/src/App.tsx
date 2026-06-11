9dd0bd2592aaba06619c7363ee5006e2ac580d42
kdev_sobreira <kauam.morais09@gmail.com>
2026-06-10 22:29:03 -0300

    feat(frontend): inicsializa projeto React com Vite, TypeScript e TailwindCSS

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">LinkBio Squad 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
