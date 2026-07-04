c638fb2dd0d02967f7822a4eefcc43caf1533473
kdev_sobreira <kauam.morais09@gmail.com>
2026-07-04 18:47:29 -0300

    feat(frontend): exportando os dados

---
+++ apps/frontend/src/data/index.ts
@@ -0,0 +1,6 @@
+export type { Badge, BadgeStatus } from "./badges";
+export { badgesMock } from "./badges";
+export { membrosMock } from "./members";
+export { statsMock } from "./stats";
+export type { Tech, TechCategory } from "./techs";
+export { techsMock } from "./techs";
